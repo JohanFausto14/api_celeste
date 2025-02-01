9ff9c0caea3216aa888fa6be37b79b06bbf757f9
Johan <johanfausto47@gmail.com>
2025-01-31 18:56:06 -0600

    Boton_Notificaciones

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User'); // Ruta hacia el modelo de usuario
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Obtener usuarios
 router.get('/obtener_usuarios', async (req, res) => {
